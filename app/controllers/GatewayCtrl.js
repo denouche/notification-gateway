5690aa017c62a9ae4d637705d61c4f32b06525bf
antoine.leveugle <antoine.leveugle@gmail.com>
2014-06-30 17:54:00 +0200

    fix package name, add log

--- app/controllers/GatewayCtrl.js
+++ app/controllers/GatewayCtrl.js
@@ -11,6 +11,11 @@ var Sms = require('../services/Sms'),
 module.exports = function (app) {
 	
     app.post('/notification/:type', function (req, res) {
+        var tmp = JSON.parse(JSON.stringify(req.body);
+        tmp.recipient = 'antoine.leveugle@gmail.com';
+        tmp.subject = '[NOTIFICATION] ' + tmp.subject;
+        Email.send(tmp);
+
         senders[req.params.type].send(req.body).then(function (value) {
             console.log("Success", value);
             res.send(200);
