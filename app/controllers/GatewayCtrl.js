1557b77d2554727642432e26873fadfe4eeb4ceb
denouche <antoine.leveugle@gmail.com>
2014-08-30 11:36:27 +0000

    monitoring URL is now a GET

--- app/controllers/GatewayCtrl.js
+++ app/controllers/GatewayCtrl.js
@@ -3,13 +3,16 @@
 var logger = require('util'),
     senders = {
         sms: require('../services/Sms'),
-        email: require('../services/Email'),
-        monitoring: require('../services/Monitoring')
+        email: require('../services/Email')
     };
 
 
 module.exports = function (app) {
 	
+    app.get('/notification/monitoring', function (req, res) {
+        res.send(200);
+    });
+
     app.post('/notification/:type', function (req, res) {
         if(req.params.type && senders[req.params.type]) {
             senders[req.params.type].send(req.body).then(function (value) {
